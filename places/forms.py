27f092942e8f6d0057ef125db395be7156c638fe
Anselmo-2010 <you@example.com>
2021-07-14 19:01:57 +0600

    added new files

---
+++ places/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.forms import fields
+from .models import Place
+
+class PlaceForm(forms.ModelForm):
+    class Meta:
+        model = Place
+        fields = ['name', 'location', 'description']
